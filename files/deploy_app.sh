99ea4cd8beecfe720317269d7bc818638ff32e2d
anderl30 <lorraine.anderson@airnz.co.nz>
2023-08-23 03:22:09 +0000

    Updated HTML Payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own text. Not too sure what to write. Some guys are numpties!!.
+  Welcome to ${PREFIX}'s app. Replace this text with your own text. Lots of guys are numpties!!.
   <!-- END -->
 
   </div>
